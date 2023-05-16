1239caaee5f831ae8468b7d6db543364c9cde27b
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-05-16 00:22:57 -0500

    Able to read training file

--- src/Sentiment_Analyzer.cpp
+++ src/Sentiment_Analyzer.cpp
@@ -1,6 +1,67 @@
 #include "Sentiment_Analyzer.h"
 
-Sentiment_Analyzer::Sentiment_Analyzer() = default;
+// default constructor
+Sentiment_Analyzer::Sentiment_Analyzer() {
+    this->trainFile = nullptr;
+    this->testFile = nullptr;
+    this->ansKey = nullptr;
+    this->outFile = nullptr;
+}
+
+Sentiment_Analyzer::Sentiment_Analyzer(char* trainFile, char* testFile, char* ansKey, char* outFile) {
+    this->trainFile = trainFile;
+    this->testFile = testFile;
+    this->ansKey = ansKey;
+    this->outFile = outFile;
+}
 
 void Sentiment_Analyzer::read_training_file() {
+    if (this->trainFile == nullptr) {
+        std::cout << "No training file was passed!" << std::endl;
+        return;
+    }
+
+    // open training file
+    std::ifstream inFS(this->trainFile);
+    if (inFS.is_open()) {
+        char* headers = new char[256];
+        char* sentiment = new char[4];
+        char* ID = new char[12];
+        char* date = new char[30];
+        char* dummy = new char[10];
+        char* username = new char[20];
+        char* tweet = new char[500];
+
+        inFS.getline(headers, 256, '\n');
+        while(!inFS.eof()) {
+            inFS.getline(sentiment, 4, ',');
+            DSString senVal(sentiment);
+
+            inFS.getline(ID, 12, ',');
+            DSString idString(ID);
+
+            inFS.getline(date, 30, ',');
+            DSString dateString(date);
+
+            inFS.getline(dummy, 10, ',');
+            inFS.getline(username, 20, ',');
+            DSString nameString(username);
+
+            inFS.getline(tweet, 500, '\n');
+            DSString tweetString(tweet);
+
+            Tweet newTweet(senVal, idString, dateString, nameString, tweetString);
+            std::cout << newTweet << std::endl;
+        }
+
+        delete[] headers;
+        delete[] sentiment;
+        delete[] ID;
+        delete[] date;
+        delete[] dummy;
+        delete[] username;
+        delete[] tweet;
+    } else {
+        std::cout << "No training file found!" << std::endl;
+    }
 }
--- src/Sentiment_Analyzer.h
+++ src/Sentiment_Analyzer.h
@@ -1,9 +1,22 @@
+#ifndef SENTIMENT_ANALYZER_SENTIMENT_ANALYZER_H
+#define SENTIMENT_ANALYZER_SENTIMENT_ANALYZER_H
+
 #include <iostream>
+#include <vector>
+
+#include "Tweet.h"
 
 class Sentiment_Analyzer {
 private:
+    char* trainFile;
+    char* testFile;
+    char* ansKey;
+    char* outFile;
 public:
     explicit Sentiment_Analyzer();
+    explicit Sentiment_Analyzer(char* trainFile, char* testFile, char* ansKey, char* outFile);
 
     void read_training_file();
 };
+
+#endif //SENTIMENT_ANALYZER_SENTIMENT_ANALYZER_H
