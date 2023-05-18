0b3d50634c363a2b906ab18b626ce33f8c72159b
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-05-17 21:18:20 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,20 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-
+    // check number of program arguments
     if (argc > 1) {
-        Sentiment_Analyzer p1(argv[1], nullptr, nullptr, nullptr);
+        Sentiment_Analyzer p1(argv[1], argv[2], nullptr, nullptr);
 
         // training phase
         p1.read_training_file();
         p1.make_classifier();
 
         // testing phase
+        p1.read_testing_file();
 
     } else {
         Catch::Session().run();
     }
+
     return 0;
 }
