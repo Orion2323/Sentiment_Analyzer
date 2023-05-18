ce5b081770487f3b808dd818e9910fbdbbacdc5b
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-05-17 21:18:16 -0500

    Update Tweet.cpp

--- src/Tweet.cpp
+++ src/Tweet.cpp
@@ -11,24 +11,68 @@ Tweet::Tweet(const DSString& sentiment, const DSString& ID, const DSString& date
     this->tweet = tweet;
 }
 
+bool Tweet::operator==(const Tweet& t) const {
+    return (this->ID == t.ID);
+}
+
+bool Tweet::operator==(const DSString& dstr) const {
+    return (this->ID == dstr);
+}
+
+bool Tweet::operator<(const Tweet& t) const {
+    return (this->ID < t.ID);
+}
+
+bool Tweet::operator<(const DSString& dstr) const {
+    return (this->ID < dstr);
+}
+
+bool Tweet::operator>(const Tweet& t) const {
+    return (this->ID > t.ID);
+}
+
+bool Tweet::operator>(const DSString& dstr) const {
+    return (this->ID > dstr);
+}
+
 DSString& Tweet::get_sentiment() {
     return this->sentiment;
+}
+
+void Tweet::set_sentiment(const DSString& sentiment) {
+    this->sentiment = sentiment;
 }
 
 DSString& Tweet::get_ID() {
     return this->ID;
 }
 
+void Tweet::set_ID(const DSString& ID) {
+    this->ID = ID;
+}
+
 DSString& Tweet::get_date() {
     return this->date;
+}
+
+void Tweet::set_date(const DSString& date) {
+    this->date = date;
 }
 
 DSString& Tweet::get_username() {
     return this->username;
 }
 
+void Tweet::set_username(const DSString& username) {
+    this->username = username;
+}
+
 DSString& Tweet::get_tweet() {
     return this->tweet;
+}
+
+void Tweet::set_tweet(const DSString& tweet) {
+    this->tweet = tweet;
 }
 
 std::vector<DSString> Tweet::get_tweet_words() {
